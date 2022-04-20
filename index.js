7c9a6f3a739f8f1cfdf651b683d6c191f7e6aa0b
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-20 12:46:38 -0500

    commit tree 6538ce9d08e6365abeac06f3622c4451956573d4

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    // console.log(i)
+    // console.log(word[i])
+    const j = word.length - 1 - i;
+    // console.log(j)
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
-
+isPalindrome('racecar')
 /* 
-  Add your pseudocode here
+  Add your pseudocode here.
+  Iterate over the data set with a for loop
+    If data(i)=data(-i)
+    return data
+
 */
 
 /*
@@ -11,15 +22,15 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
 
-module.exports = isPalindrome;
+// module.exports = isPalindrome;
